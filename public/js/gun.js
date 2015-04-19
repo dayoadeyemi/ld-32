3ce927e58bd8f764dabd96ab1bf3b107d5839d1c
Dayo Adeyemi <dayoadeyemi1991@hotmail.com>
2015-04-19 09:32:40 +0100

    updates

--- public/js/gun.js
+++ public/js/gun.js
@@ -5,6 +5,8 @@ var lastStartedCharging = 0
 
 var BULLET_VELOCITY = 500;
 
+var gunSprite;
+
 var switchType = function() {
 	if(type == GunType.ENLARGE) {
 		type = GunType.SHRINK;
@@ -22,22 +24,44 @@ var startCharging = function(game) {
 }
 
 var fire = function(game, player, mouseX, mouseY) {
+	var x_diff = (mouseX + game.camera.x) - player.x;
+	var y_diff = (mouseY + game.camera.y) - player.y;
+	var magnitude = Math.sqrt(x_diff * x_diff + y_diff * y_diff);
+	x_diff /= magnitude, y_diff /= magnitude;
+
 	if(!isCharging) throw "the toys out the pram";
 	isCharging = false;
 	console.log("We were charging for " + (game.time.now - lastStartedCharging) + "seconds");
-	bullet = game.add.sprite(player.x, player.y, 'bullet');
+	bullet = game.add.sprite(gunSprite.x + x_diff*64, gunSprite.y + y_diff*64, 'bullet');
 	game.physics.arcade.enable(bullet);
+	bullet.rotation = gunSprite.rotation;
+	bullet.anchor.setTo(0.5, 0.5)
 
-	var x_diff = (mouseX + game.camera.x) - player.x;
-	var y_diff = mouseY - player.y;
-	var magnitude = Math.sqrt(x_diff * x_diff + y_diff * y_diff);
-	var mag_scaling_factor = BULLET_VELOCITY / magnitude;
-
-	bullet.body.velocity.x = x_diff * mag_scaling_factor;
-	bullet.body.velocity.y = y_diff * mag_scaling_factor;
+	bullet.body.velocity.x = x_diff * BULLET_VELOCITY;
+	bullet.body.velocity.y = y_diff * BULLET_VELOCITY;
 	return bullet;
 }
 
+
+function getSprite(game){
+	gunSprite = game.add.sprite(0, 0, 'gun');
+	gunSprite.anchor.setTo(0.05, 0.6)
+	gunSprite.animations.add('fire', [0, 1, 2, 3, 2, 1, 0], 20, false);
+	return gunSprite;
+}
+
+function update(game, player) {
+	var rot = game.physics.arcade.angleToPointer(player)
+
+	gunSprite.scale.y = Math.abs(rot) < Math.PI/2 ? 1 : -1;
+
+	gunSprite.rotation = rot;
+	gunSprite.position.x = player.body.position.x+32;
+	gunSprite.position.y = player.body.position.y+32;
+}
+
+module.exports.update = update;
+module.exports.getSprite = getSprite;
 module.exports.switchType = switchType;
 module.exports.startCharging = startCharging;
 module.exports.fire = fire;
--- public/js/main.js
+++ public/js/main.js
@@ -2,8 +2,9 @@ var _ = require('highland');
 var R = require('ramda');
 var input = require('./input.js');
 var gun = require('./gun.js')
-var platforms, player, map, layer;
+var platforms, player, gun, map, layer, gunSprite, boxes, inputState = input.INITIAL_STATE;
 var width, height, lastTime;
+var GRAVITY = 700;
 
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
@@ -12,63 +13,90 @@ function preload() {
   game.load.tilemap('map', '../resources/maps/test.json', null, Phaser.Tilemap.TILED_JSON);
   game.load.spritesheet('tiles', '../resources/tiles.png', 32, 32);
 
-  game.load.image('star', '../resources/star.png');
   game.load.spritesheet('player', '../resources/player.png', 72, 72);
+  game.load.spritesheet('gun', '../resources/gun.png', 128, 128);
   game.load.image('bullet', '../resources/pl-bullet.png');
+  game.load.image('box', '../resources/box.png');
 }
 
 function create() {
   game.stage.backgroundColor = '#3399FF';
-  game.physics.startSystem(Phaser.Physics.ARCADE);
+  game.physics.startSystem(Phaser.Physics.P2);
 
   map = game.add.tilemap('map');
   map.addTilesetImage('tiles');
-  layer = map.createLayer('Tile Layer 1');
-  map.setCollisionBetween(1, 6);
-  layer.resizeWorld();
+  map.addTilesetImage('box');
 
+  boxes = game.add.group();
+  boxes.enableBody = true;
+  map.createFromObjects('boxes', 9, 'box', 0, true, false, boxes);
+  boxes.setAll('body.bounce.y', 0.2);
+  boxes.setAll('body.gravity.y', GRAVITY);
+  boxes.setAll('modSize', 1);
+  boxes.callAll('player.anchor.setTo', 0.5, 0.5);
+
+  layer = map.createLayer('Tile Layer 1');
+  layer.resizeWorld();
   layer.enableBody = true;
+
+  map.setCollisionBetween(1, 6);
 
   player = game.add.sprite(200, game.world.height - 400, 'player');
   game.physics.arcade.enable(player);
   player.body.bounce.y = 0.2;
-  player.body.gravity.y = 700;
+  player.body.gravity.y = GRAVITY;
   player.body.collideWorldBounds = true;
   player.animations.add('walk', [0, 1, 2, 3, 4], 10, true);
   player.anchor.setTo(0.5, 0.5)
   game.camera.follow(player);
 
-  input().each(function(state){
-    if (state.left){
-      player.body.velocity.x = -150;
-      player.scale.x = -1
-      player.animations.play('walk');
-    } else if (state.right){
-      player.body.velocity.x = 150;
-      player.scale.x = 1
-      player.animations.play('walk');
-    } else {
-      player.body.velocity.x = 0;
-      player.animations.stop();
-      player.frame = 0;
-    }
+  gunSprite = gun.getSprite(game)
 
-    if (state.space) {
+  input().each(function(s){
+    inputState = s;
+
+    if (inputState.space) {
       gun.switchType();
     }
 
-    if(state.mousedown) {
+    if(inputState.mousedown) {
       gun.startCharging(game)
-    } else if(state.mouseup) {
-      gun.fire(game, player, state.x, state.y)
+    } else if(inputState.mouseup) {
+      gun.fire(game, player, inputState.x, inputState.y)
+      gunSprite.animations.play('fire');
     }
 
-    if (state.up && player.body.onFloor()){
-        player.body.velocity.y = -350;
+    if (inputState.up && player.body.onFloor()){
+        player.body.velocity.y = -GRAVITY/2;
     }
   })
 }
 
 function update() {
+  game.physics.arcade.collide(boxes, layer);
+  boxes.forEach(function(box) {
+    if (box.body.onFloor()) {
+      box.body.drag.setTo(100000);
+    } else {
+      box.body.drag.setTo(0);
+    }
+    box.scale = box.modSize;
+  }, this);
+
   game.physics.arcade.collide(player, layer);
+  game.physics.arcade.collide(player, boxes);
+  gun.update(game, player, inputState)
+  if (inputState.left){
+    player.body.velocity.x = -150;
+    player.scale.x = -1;
+    player.animations.play('walk');
+  } else if (inputState.right){
+    player.body.velocity.x = 150;
+    player.scale.x = 1;
+    player.animations.play('walk');
+  } else {
+    player.body.velocity.x = 0;
+    player.animations.stop();
+    player.frame = 0;
+  }
 }
